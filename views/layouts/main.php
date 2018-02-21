98c05ef05349a55744b266fa75935c9d1e164a11
s.kainovs <s.kainovs@gmail.com>
2018-02-21 03:38:36 -0500

    Commit messageq1231

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -35,27 +35,35 @@ AppAsset::register($this);
             'class' => 'navbar-default navbar-fixed-top',
         ],
     ]);
+
+
+           $menuItems = [ // Те, которые будут отображаться всегда
+        ['label' => 'Главная', 'url' => ['/site/index']],
+        ['label' => 'Контакт', 'url' => ['/site/contact']],
+    ];
+
+    if(Yii::$app->user->isGuest) // Если пользователь не авторизован
+    {
+        $menuItems[] = ['label' => 'Зарегистрироватся', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => 'Войти', 'url' => ['/site/login']];
+    }
+    else
+    {
+        $menuItems[] = ['label' => 'Статьи', 'url' => ['/post']];
+        $menuItems[] = '<li>'
+            . Html::beginForm(['/site/logout'], 'post') // Форма логаута, смотрим виджет ActiveForm
+            . Html::submitButton(
+                'Выйти (' . Yii::$app->user->identity->username . ')',
+                ['class' => 'btn btn-link logout']
+            )
+            . Html::endForm()
+            . '</li>';
+    }
+
+
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            ['label' => 'Зарегистрироватся', 'url' => ['/site/signup']],
-            Yii::$app->user->isGuest ? (
-
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
+        'items' => $menuItems,
     ]);
     NavBar::end();
     ?>
